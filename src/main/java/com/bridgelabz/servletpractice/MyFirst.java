4d7ad0612087782ca7a88140dcf2f6f757315d82
unknown <gayoyawalkar@gmail.com>
2021-12-09 19:46:43 +0530

    Added:MyFirst servlet

---
+++ src/main/java/com/bridgelabz/servletpractice/MyFirst.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.servletpractice;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+public class MyFirst extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h3>Hello World My First Servlet</h3>");
+		out.close();
+	}
+}
